f8ec0b797e8bfb59761d460d844518f5153fbaa7
Arthur Santana <arthur.santana@jpvtech.com.br>
2022-06-28 20:41:10 -0300

    refactor: alterar arquivos para typeScript

--- backend/src/config/env/dev.js
+++
@@ -1,3 +0,0 @@
-const dbConnect = 'mongodb+srv://thurzao:280299@thur.2b0qb2s.mongodb.net/token-API?';
-
-export default dbConnect;
---
+++ backend/src/config/env/dev.ts
@@ -0,0 +1,3 @@
+const dbConnect: string = 'mongodb+srv://thurzao:280299@thur.2b0qb2s.mongodb.net/token-API?';
+
+export default dbConnect;
